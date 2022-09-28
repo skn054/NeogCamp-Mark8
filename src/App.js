733c8f34a1e99caf97fde7f383cae662341aeff8
SaiKiran <skn054@gmail.com>
2022-09-29 02:01:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "./styles.css";
+import { useState } from "react";
+
+var emojiDic = {
+  "🐼": "Panda",
+  "🎻": "Violin",
+  "😴": "Sleeping",
+  "👻": "Ghost",
+  "🌈": "Rainbow",
+  "🐒": "Monkey",
+  "✨": "Sparkles",
+  "🌹": "Rose",
+  "🦋": "Butterfly",
+  "🔥": "Fire",
+  "🍁": "Maple Leaf",
+  "📷": "Camera",
+  "🎈": "Balloon",
+  "💣": "Bomb",
+  "🐬": "Dolphin",
+  "🪁": "Kite",
+  "🔖": "Bookmark",
+  "🐸": "Frog",
+  "🔔": "Bell",
+  "👽": "Alien",
+  "☀️": "Sun",
+  "🎸": "Guitar"
+};
+
+var emojiList = Object.keys(emojiDic);
+
+export default function App() {
+  var [guessing, setGuessing] = useState("");
+  function onChangeListener(event) {
+    console.log("onChangeListener called");
+    console.log(event.target.value);
+    guessing = emojiDic[event.target.value];
+    if (guessing === undefined && event.target.value !== "") {
+      guessing = "emoji Not Found";
+    }
+
+    setGuessing(guessing);
+  }
+
+  function onClickListener(item) {
+    console.log(item);
+    setGuessing(emojiDic[item]);
+  }
+
+  return (
+    <div className="App">
+      <input onChange={onChangeListener} />
+      <h2>{guessing}</h2>
+      {emojiList.map((item) => {
+        return (
+          <span key={item} onClick={() => onClickListener(item)}>
+            {item}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
